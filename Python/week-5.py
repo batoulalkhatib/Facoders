7db9d0a6318c3cbc54a9bb02a409e16d6dca6b53
batoulalkhatib <44109232+batoulalkhatib@users.noreply.github.com>
2018-11-11 00:07:56 -0800

    Update week-5.py

--- Python/week-5.py
+++ Python/week-5.py
@@ -1,7 +1,7 @@
 s1= ['Ahmad', 18, 17, 19.5, 8, 25]
 s2= ['Sami', 20, 20, 19, 9, 28]
 s3= ['Faris', 14.5, 16, 13, 7, 23]
-name=input("Enter student/'s name: ")
+name=input("Enter student\'s name: ")
 if name ==s1[0] :
     print(name,sum(s1[1:7]))
 elif name ==s2[0] :
