2a46794e924b1c255b937f44ef2a68d192eda2bc
batoulalkhatib <batoulkh@hotmail.com>
2018-11-17 01:31:09 -0800

    last

--- Python/week-5.py
+++ Python/week-5.py
@@ -1,7 +1,7 @@
 s1= ['Ahmad', 18, 17, 19.5, 8, 25]
 s2= ['Sami', 20, 20, 19, 9, 28]
 s3= ['Faris', 14.5, 16, 13, 7, 23]
-name=input("Enter student/'s name: ")
+name=input("Enter student\'s name: ")
 if name ==s1[0] :
     print(name,sum(s1[1:7]))
 elif name ==s2[0] :
@@ -9,4 +9,4 @@ elif name ==s2[0] :
 elif name ==s3[0] :
     print(name,sum(s3[1:7]))
 else:
-    print("Student is not recorded")
+     print("Student is not recorded",0)
